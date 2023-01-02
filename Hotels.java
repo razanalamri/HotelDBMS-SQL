fe4281e5490516d7f73d9643143845f0e799b0fa
razanalamri <razan.f96@outlook.com>
2023-01-02 17:05:39 +0400

    first try

--- Hotels.java
+++ Hotels.java
@@ -1,8 +1,13 @@
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
+
 
 public class Hotels {
 	
@@ -12,7 +17,8 @@ public class Hotels {
 	 String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelDBMS;encrypt=true;trustServerCertificate=true";
      String user = "sa";
      String pass = "root";
-     Scanner scanner = new Scanner(System.in);
+     Scanner scanner1=new Scanner(System.in);
+    
      
      //creating first table
      String Sql = "CREATE TABLE Hotels " + "(id INTEGER PRIMARY KEY, " + " hotel_name VARCHAR(50) NOt NULL, "
@@ -45,9 +51,64 @@ public class Hotels {
      catch (Exception ex) {
 
          System.err.println(ex);
+     }insertIntoTable();
      }
+         
+   
+     
+     public static void insertIntoTable(){
+    	 
+    	 String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelDBMS;encrypt=true;trustServerCertificate=true";
+         String user = "sa";
+         String pass = "root";
+         
+    	 Scanner sa=new Scanner(System.in);
+     	System.out.println("Please type the number of rows");
+         int userinput =sa.nextInt();
+         String hotel_name="Plaza";
+         
+         String hotel_location="Seeb";
+         
+         String created_date="1996-03-01";
+         
+         String updated_date="1996-08-02";
+         
+         boolean is_Active=false;
+         Random rn = new Random();
+         Integer numberToAdd = rn.nextInt(100);
+         for(int i=0; i<=userinput;i++) {
+         String sql = "insert into Hotels values ("+i+numberToAdd+", '"+hotel_name+i+"', '"+hotel_location+i+"','"+created_date+i+"','"+updated_date+", '"+is_Active+"')";
+    	 
+         Connection con = null;
+         try {
 
+             Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+             // Registering drivers
+             DriverManager.registerDriver(driver);
 
-	
-	
-}}
+             // Reference to connection interface
+             con = DriverManager.getConnection(url, user,
+                     pass);
+
+             // Creating a statement
+             Statement st = con.createStatement();
+
+             // Executing query
+             int m = st.executeUpdate(sql);
+             if (m >=  0)
+                 System.out.println("Hotels table Created successfully");
+             else
+                 System.out.println("Creation failed");
+
+             
+             con.close();
+         }
+         catch (Exception ex) {
+
+             System.err.println(ex);
+    	 
+     }}
+    
+     
+     
+     }}
--- Main.java
+++ Main.java
@@ -6,9 +6,46 @@ import java.util.Scanner;
 
 public class Main {
 	 public static void main(String[] args) {
+		 Hotels hotel=new Hotels();
+		 Scanner scanner = new Scanner(System.in);
 	
 	  
-	             
+		    System.out.println("Choose One Option :");
+		    
+	        System.out.println("1-Insert 10,000 hotels");
+	        System.out.println("2-Insert 1 hotel");
+	        System.out.println("3-Print 10 hotels");
+	        System.out.println("4-Add a new Subject");
+	        System.out.println("5-Make first 10 hotels 'is_Active' = false");
+	        System.out.println("6-Print hotel information by user input");
+	        int option=scanner.nextInt();
+	        
+	        
+	        switch(option) {
+	        
+	        case 1:
+	        	
+	        	
+	             break;
+	        case 2:
+	  
+	             break;
+	        case 3:
+	     
+	             break;
+	        case 4:
+	        	
+	             break;
+	        case 5:
+	        
+	             break;
+		       
+	        case 6:
+		        
+	   
+	        		         	     
+
+	        }   
 	             
 	            
 	  	           
