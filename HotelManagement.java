33cced18dd043ee6a6c7d5c0cb55d285659c0357
razanalamri <razan.f96@outlook.com>
2023-01-10 09:33:52 +0400

    Rooms where guests are paying more than 1000

--- HotelManagement.java
+++ HotelManagement.java
@@ -10,7 +10,8 @@ public class HotelManagement {
 	
 	  public static void main (String[] args) {
 		 
-		  endByE();
+		  //endByE();
+		  guestsPayingmorethan1000(); 
 		  
 		  
 		  
@@ -59,4 +60,54 @@ public class HotelManagement {
 
 	                     System.err.println(ex);
 	                 }
+	                 }
+	 
+	 public static void guestsPayingmorethan1000() {
+		 
+		 	
+		 String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelDBMS;encrypt=true;trustServerCertificate=true";
+	     String user = "sa";
+	     String pass = "root";
+	     Scanner scanner1=new Scanner(System.in);
+	    
+	            		
+	            		
+	                 
+	                 Connection con = null;
+	                 try {
+
+	                     Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+	                     // Registering drivers
+	                     DriverManager.registerDriver(driver);
+
+	                     // Reference to connection interface
+	                     con = DriverManager.getConnection(url, user,
+	                             pass);
+
+	                     // Creating a statement
+	                     Statement st = con.createStatement();
+	                     
+	                     
+	                     String Sql ="SELECT guest_name FROM Guests INNER JOIN Rooms ON Guests.hotel_id=Rooms.hotel_id WHERE  guest_payment_amount>1000";
+	                     	
+	                     ResultSet result=st.executeQuery(Sql);
+	        		     while(result.next()) {
+	        		    	 String guestname=result.getString("guest_name");
+	        		    
+	           	               System.out.println(guestname);
+	           	 
+
+	       
+	                
 	                 }}
+	                 catch (Exception ex) {
+
+	                     System.err.println(ex);
+	                 }
+	                 }
+
+
+
+
+
+}
